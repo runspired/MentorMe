794c08d1b3908aabfd85865b8b9630ea403c3b6c
Chris Thoburn <runspired@gmail.com>
2016-05-09 21:09:14 -0500

    fun things

--- app/components/tap-master-2000/component.js
+++ app/components/tap-master-2000/component.js
@@ -2,20 +2,50 @@ import Ember from 'ember';
 
 const {
   Component,
+  computed,
   observer
   } = Ember;
 
 export default Component.extend({
 
   madness: 0,
-  madnessAcceleration: 1.1,
+  userAnger: computed('rounded', function() {
+    let madness = this.get('rounded');
+
+    if (madness < 25) {
+      return 'meh';
+    }
+
+    if (madness < 50) {
+     return 'so angry my hair is raised';
+    }
+
+    if (madness < 100) {
+      return 'so angry my blood is boiling!';
+    }
+
+    if (madness < 150) {
+      return 'so angry TSA won\'t let me through';
+    }
+
+  }),
+
+  rounded: computed('madness', function() {
+    return Math.round(this.get('madness'));
+  }),
+  madnessAcceleration: 1.05,
 
   spinAirplane: observer('madness', function() {
-    let madness = Math.round(this.get('madness') * 360);
+    let madness = Math.round(this.get('madness') * 360 * 10);
 
-    this.$('.spinner').velocity({
-      duration: 300,
+    this.$('.spinner-left').velocity({
+      duration: 1500,
       rotateZ: `${madness}deg`
+    });
+
+    this.$('.spinner-right').velocity({
+      duration: 1500,
+      rotateZ: `-${madness}deg`
     });
   }),
 
@@ -23,12 +53,11 @@ export default Component.extend({
     beMad() {
       let madness = this.get('madness');
       if (!madness) {
-        madness = 10;
+        madness = 5;
       } else {
         madness *= this.get('madnessAcceleration');
       }
 
-      console.log('MAD!', madness);
       this.set('madness', madness);
     }
   }
