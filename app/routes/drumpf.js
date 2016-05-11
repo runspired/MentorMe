d86ee0e53b9790ca65f08b006624583d8674f065
haileb <bhaile@uchicago.edu>
2016-05-10 23:25:33 -0500

    app/index.html

---
+++ app/routes/drumpf.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+});
+
+
+        $(document).ready(function(){
+            $('.menu').click(function(){
+                $(this).toggleClass('active');
+                $(this).parents('.container').find('.navigation').slideToggle('fast');
+            });
+            $('.actions').hover(
+                function(){
+                    $(this).find('.btn').removeClass('success').addClass('danger').text('Unfollow');
+                },
+                function(){
+                    $(this).find('.btn').removeClass('danger').addClass('success').text('Following');
+                }
+            );
+        });
